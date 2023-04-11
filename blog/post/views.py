96e32abe0bb04647db5e08adf162e9ecac7fa985
asylbekovatilee <asylbekovanasyikat@gmail.com>
2023-04-11 12:03:31 +0600

    post listing with templates

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from post.models import Post
 
-# Create your views here.
+def posts_list(request):
+    queryset = Post.objects.all()
+    return render(request, 'listing.html', {"posts":queryset})
+
